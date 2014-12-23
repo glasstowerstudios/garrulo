66d3f14f0bcffac908b387e79bae28796ed6cec1
Scott Johnson <jaywir3@gmail.com>
2014-12-23 16:54:03 -0600

    (#1): Add a settings activity to control whether or not NFC is enabled.

--- Garrulo/src/main/java/com/glasstowerstudios/garrulo/ui/GarruloMainActivity.java
+++ Garrulo/src/main/java/com/glasstowerstudios/garrulo/ui/GarruloMainActivity.java
@@ -45,6 +45,11 @@ public class GarruloMainActivity
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_garrulo_main, menu);
@@ -60,7 +65,8 @@ public class GarruloMainActivity
 
         switch (id) {
             case R.id.action_settings:
-                return true;
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
             case R.id.action_test:
                 runSpeakingTest();
                 break;
